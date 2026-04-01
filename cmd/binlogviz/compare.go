ca2e65e9a79c0108c5d9eb50fcb2ffe757956707
Fan() <18501341937@163.com>
2026-04-01 13:14:34 +0800

    feat: wire compare command execution

--- cmd/binlogviz/compare.go
+++ cmd/binlogviz/compare.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	comparepkg "binlogviz/internal/compare"
 )
 
 type compareOptions struct {
@@ -20,10 +22,39 @@ func newCompareCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			switch opts.format {
 			case "text", "json", "html":
-				return fmt.Errorf("compare execution not implemented")
 			default:
 				return fmt.Errorf("unsupported compare format: %s", opts.format)
 			}
+
+			current, err := comparepkg.LoadReport(args[0])
+			if err != nil {
+				return fmt.Errorf("load current report %s: %w", args[0], err)
+			}
+			baseline, err := comparepkg.LoadReport(args[1])
+			if err != nil {
+				return fmt.Errorf("load baseline report %s: %w", args[1], err)
+			}
+
+			result := comparepkg.BuildCompareResult(current, baseline)
+
+			var output string
+			switch opts.format {
+			case "json":
+				output, err = comparepkg.RenderJSON(result)
+			case "html":
+				output, err = comparepkg.RenderHTML(result)
+			default:
+				output, err = comparepkg.RenderText(result)
+			}
+			if err != nil {
+				return fmt.Errorf("render compare output (%s): %w", opts.format, err)
+			}
+
+			_, err = fmt.Fprint(cmd.OutOrStdout(), output)
+			if err != nil {
+				return fmt.Errorf("write compare output: %w", err)
+			}
+			return nil
 		},
 	}
 
